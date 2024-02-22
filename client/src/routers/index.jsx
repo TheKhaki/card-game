25ef6ef02acb060142f9c3157d9394c6f008692c
WAHYU_WHISNU_WARDHANA <whisnuwardhana29@gmail.com>
2024-02-22 09:47:06 +0700

    bisa buat room

--- client/src/routers/index.jsx
+++ client/src/routers/index.jsx
@@ -1,51 +1,49 @@
-import {createBrowserRouter} from 'react-router-dom'
-import Register from '../pages/Register'
-import Login from '../pages/Login'
-import Homepage from '../pages/Homepage'
-import Game from '../pages/Game'
-import { Route, Routes } from 'react-router-dom'
-import{Outlet} from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import Homepage from "../pages/Homepage";
+import Game from "../pages/Game";
+import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const router = createBrowserRouter(
-    // createRoutesFromElements(
-    //     <Route path='/' Component={Homepage} />
-    // ),
-    [
-
+  // createRoutesFromElements(
+  //     <Route path='/' Component={Homepage} />
+  // ),
+  [
     {
-        path: "/register",
-        element: <Register />,
+      path: "/register",
+      element: <Register />,
     },
     {
-        path : "/login",
-        element : <Login />
-    }
-    // {
-    //     path: '/',
-    //     element: <Homepage/>
-    // },
-    // {
-    //     path: '/play',
-    //     element: <Homepage/>
-    // },
-    ,
-    {
-        path : '*',
-        element: [
-            <Routes>
-                <Route path='/' exact Component={Homepage}/>
-            </Routes>
-        ]
+      path: "/login",
+      element: <Login />,
     },
     {
-        path : '/play/*',
-        element: [
-            <Routes>
-                <Route path='/play' exact  Component={Game}/>
-            </Routes>
-        ]
-    }
+      path: "/",
+      element: <Homepage />,
+    },
+    {
+      path: "/play",
+      element: <Game />,
+    },
+    // {
+    //     path : '*',
+    //     element: [
+    //         <Routes>
+    //             <Route path='/' element={<Homepage />}/>
+    //         </Routes>
+    //     ]
+    // },
+    // {
+    //     path : '/play/*',
+    //     element: [
+    //         <Routes>
+    //             <Route path='/play' element={<Game />}/>
+    //         </Routes>
+    //     ]
+    // }
+  ]
+);
 
-])
-
-export default router
+export default router;
--- server/app.js
+++ server/app.js
@@ -1,30 +1,78 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = 3000;
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+const cors = require("cors");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const router = require("./routers");
+const server = createServer(app);
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const cors = require('cors')
-const {createServer} = require('node:http')
-const { Server } = require('socket.io');
-const router = require('./routers');
-const server = createServer(app)
-app.use(cors())
-app.use(express.urlencoded({extended : true}))
-app.use(express.json())
-
-app.use( router )
+app.use(router);
 
 const io = new Server(server, {
-    cors : {
-        origin : "http://localhost:5173"
-    }
-})
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
-io.on('connection', (socket) => {
-    
-})
+io.on("connection", (socket) => {
+  console.log(socket);
+  socket.on("join", (payload, callback) => {
+    let numberOfUsersInRoom = getUsersInRoom(payload.room).length;
+
+    const { error, newUser } = addUser({
+      id: socket.id,
+      name: numberOfUsersInRoom === 0 ? "Player 1" : "Player 2",
+      room: payload.room,
+    });
+
+    if (error) return callback(error);
+
+    socket.join(newUser.room);
+
+    io.to(newUser.room).emit("roomData", {
+      room: newUser.room,
+      users: getUsersInRoom(newUser.room),
+    });
+    socket.emit("currentUserData", { name: newUser.name });
+    callback();
+  });
+
+  socket.on("initGameState", (gameState) => {
+    const user = getUser(socket.id);
+    if (user) io.to(user.room).emit("initGameState", gameState);
+  });
+
+  socket.on("updateGameState", (gameState) => {
+    const user = getUser(socket.id);
+    if (user) io.to(user.room).emit("updateGameState", gameState);
+  });
+
+  socket.on("sendMessage", (payload, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", {
+      user: user.name,
+      text: payload.message,
+    });
+    callback();
+  });
+
+  socket.on("disconnected", () => {
+    const user = removeUser(socket.id);
+    if (user)
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+  });
+});
 
 server.listen(port, () => {
-    console.log(`server start on ${port}`);
-})
+  console.log(`server start on ${port}`);
+});
 
-module.exports = app
+module.exports = app;
